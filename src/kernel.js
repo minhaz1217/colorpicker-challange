4440616edb13baaff4fd8383124df0875ba6cae5
Minhaz <minhaz1217@gmail.com>
2024-01-17 01:24:22 +0600

    color picker shade is working

--- src/kernel.js
+++ src/kernel.js
@@ -6,4 +6,20 @@ export const kernelFunction = function (width, height, hue) {
   const y = Math.floor(i / (height * 4));
   const x = Math.floor(i / 4 - y * width);
   const channel = i % 4;
+  const xRangeChangedValue = 255 - (x * 255) / width;
+  const yRangeChangedValue = 255 - (y * 255) / height;
+
+  if (channel === 0) {
+    // this is the red channel
+    return yRangeChangedValue;
+  } else if (channel === 1) {
+    // this is the green channel
+    return xRangeChangedValue - (y * 255) / height;
+  } else if (channel === 2) {
+    // this is the blue channel
+    return xRangeChangedValue - (y * 255) / height;
+  } else {
+    // this is the alpha channel
+    return 255;
+  }
 };
